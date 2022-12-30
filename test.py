9920d04e76f574539315519512dfeb2fd7994f05
Michael Lee <mhykol2114@gmail.com>
2022-12-29 19:52:06 -0800

    refactor

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ game_board = [[4, 2, 2, 4],
 user_input = 'a'
 test_board1 = str(game_board)
 
+
 def move(game_board, user_input):
     if user_input == 'a':
-        #shift to the left
+        # shift to the left
         for row in game_board:
             counter = 0
             while 0 in row:
@@ -17,7 +18,7 @@ def move(game_board, user_input):
                 counter += 1
             for i in range(counter):
                 row.append(0)
-            #combine logic
+            # combine logic
             for i in range(len(row) - 1):
                 if row[i] == row[i + 1]:
                     row[i + 1] += row[i + 1]
@@ -25,7 +26,7 @@ def move(game_board, user_input):
                     row.append(0)
 
     if user_input == 'd':
-        #shift to the right
+        # shift to the right
         for j, row in enumerate(game_board):
             row = row[::-1]
             counter = 0
@@ -34,22 +35,22 @@ def move(game_board, user_input):
                 counter += 1
             for i in range(counter):
                 row.append(0)
-            #combine logic
+            # combine logic
             for i in range(len(row) - 1):
                 if row[i] == row[i + 1]:
                     row[i + 1] += row[i + 1]
                     row.remove(row[i])
                     row.append(0)
             game_board[j] = row[::-1]
-    
+
     if user_input == 'w':
-        #rotate counter clockwise
+        # rotate counter clockwise
         rotated_board = [[], [], [], []]
         for row in game_board:
             for i in range(len(row)):
                 rotated_board[i].append(row[i])
         rotated_board = rotated_board[::-1]
-        #shift to the left
+        # shift to the left
         for row in rotated_board:
             counter = 0
             while 0 in row:
@@ -57,13 +58,13 @@ def move(game_board, user_input):
                 counter += 1
             for i in range(counter):
                 row.append(0)
-            #combine logic
+            # combine logic
             for i in range(len(row) - 1):
                 if row[i] == row[i + 1]:
                     row[i + 1] += row[i + 1]
                     row.remove(row[i])
                     row.append(0)
-        #rotate clockwise? honestly idek anymore ive been working all day on this
+        # rotate clockwise? honestly idek anymore ive been working all day on this
         rotated_board2 = [[], [], [], []]
         for row in rotated_board:
             for i in range(len(row)):
@@ -74,13 +75,13 @@ def move(game_board, user_input):
                 game_board[j][k] = new_board[j][k]
 
     if user_input == 's':
-        #rotate counter clockwise?
+        # rotate counter clockwise?
         rotated_board = [[], [], [], []]
         for row in game_board:
             for i in range(len(row)):
                 rotated_board[i].append(row[i])
         rotated_board = rotated_board[::-1]
-        #shift to the right
+        # shift to the right
         for j, row in enumerate(rotated_board):
             row = row[::-1]
             counter = 0
@@ -89,14 +90,14 @@ def move(game_board, user_input):
                 counter += 1
             for i in range(counter):
                 row.append(0)
-            #combine logic
+            # combine logic
             for i in range(len(row) - 1):
                 if row[i] == row[i + 1]:
                     row[i + 1] += row[i + 1]
                     row.remove(row[i])
-                    row.append(0)       
+                    row.append(0)
             game_board[j] = row[::-1]
-        #rotate clockwise? honestly idek anymore ive been working all day on this
+        # rotate clockwise? honestly idek anymore ive been working all day on this
         rotated_board2 = [[], [], [], []]
         for row in game_board:
             for i in range(len(row)):
@@ -106,6 +107,7 @@ def move(game_board, user_input):
             for k in range(4):
                 game_board[j][k] = new_board[j][k]
     return game_board
+
 
 move(game_board, user_input)
 
